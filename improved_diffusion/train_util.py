c28b465cfd4b58a699d258668775ebade9906619
tungnthust <thanhtungbka1@gmail.com>
2023-08-11 01:19:13 +0700

    add direction loss

--- improved_diffusion/train_util.py
+++ improved_diffusion/train_util.py
@@ -365,4 +365,4 @@ def log_loss_dict(diffusion, ts, losses, step):
         for sub_t, sub_loss in zip(ts.cpu().numpy(), values.detach().cpu().numpy()):
             quartile = int(4 * sub_t / diffusion.num_timesteps)
             logger.logkv_mean(f"{key}_q{sub_t}", sub_loss)
-            logger.comet_logger.log_metric(f"{key}_q{quartile}", sub_loss, step)
+            # logger.comet_logger.log_metric(f"{key}_q{quartile}", sub_loss, step)
